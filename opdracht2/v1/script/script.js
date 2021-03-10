6e6e1a1761f78d0dd0421affe88e846adeda165a
AronPelgrim <74137185+AronPelgrim@users.noreply.github.com>
2021-03-10 13:16:13 +0100

    Add files via upload

--- opdracht2/v1/script/script.js
+++ opdracht2/v1/script/script.js
@@ -1,9 +1,13 @@
-const options = {
-  threshold: [0.1]
+//Een threshold van 0.1 is dat de pagina 50% zichtbaar moet zijn voordat de animatie wordt geactiveerd
+let options = {
+  threshold: [0.5]
 };
-const observer = new IntersectionObserver(onEntry, options);
-const elements = document.querySelectorAll('section, article');
-
+//Waarnemen of de threshold wordt overschreden, zo ja wordt functie onEntry geactiveerd
+let observer = new IntersectionObserver(onEntry, options);
+//alle sections en artciles van de html wordt in let elements gestopt
+//alle sections en artciles van de html wordt in let elements gestopt
+let elements = document.querySelectorAll('section, article');
+//functie die wordt geactiveerd als de threshold is overschreden, isIntersecting is een boolean die op dat moment true geeft, op dat moment krijgt de section in kwestie class 'visible'
 function onEntry(entry) {
   entry.forEach((change) => {
     if(change.isIntersecting) {
@@ -11,7 +15,7 @@ function onEntry(entry) {
     }
   });
 }
-
+//observeerd alle domelementen doordat de observer door elements looped
 for (let elm of elements) {
   observer.observe(elm);
 }
